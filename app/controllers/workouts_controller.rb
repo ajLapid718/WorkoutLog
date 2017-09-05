f677db4c96aad84df4059bf3d768a705fb881b5b
Allan James Lapid <ajLapid718@gmail.com>
2017-09-04 23:36:30 -0400

    Config routes for workouts, set root path, and partially setup the workouts_controller CRUD actions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,34 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:edit, :update, :show, :destroy ]
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def workout_params
+  end
+
+  def find_workout
+  end
+
 end
